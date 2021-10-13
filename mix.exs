e06fb61f309479f93fa547f3afe217151834a77f
Tim Smart <tim.smart@arisechurch.com>
2021-10-13 23:17:40 +1300

    0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bubblewrap.Mixfile do
   def project do
     [
       app: :bubblewrap,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.4",
       description: description(),
       package: package(),
